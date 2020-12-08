d7fe14b2d5c8cd1c66f748bfe46a320fe1b44306
j32u4ukh <j32u4ukh@gmail.com>
2020-12-08 21:44:40 +0800

    D9

--- OpenCVLearning/D9.py
+++ OpenCVLearning/D9.py
@@ -5,7 +5,10 @@ from utils.opencv import showImages
 
 """Working directory: CupoyLearning
 
+取得 SIFT 特徵
 
+* 轉成灰階圖片
+* 需要額外安裝 OpenCV 相關套件
 """
 
 path = "image/lena.png"
@@ -14,64 +17,23 @@ img = cv2.imread(path)
 # 轉為灰階圖片
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# region 教學
+# 建立 SIFT 物件
 """
-模糊: 透過 Gaussian Filter 實作模糊操作
+如果要透過 OpenCV 使用 SIFT 的話必須要額外安裝擴充的函式庫
+
+為了避免版本問題，我們會指定安裝版本
+
+pip install opencv-contrib-python==3.4.2.16
+
+# 處理權限問題: ERROR: Could not install packages due to an EnvironmentError: [WinError 5] 存取被拒。
+pip install --user opencv-contrib-python==3.4.2.16
 """
+sift = cv2.xfeatures2d.SIFT_create()
 
-img_blur = img.copy()
+# 取得 SIFT 關鍵點位置
+keypoints = sift.detect(gray, None)
 
-# 重複多次 Gaussian 模糊的操作來加深模糊的程度
-img_blur1 = cv2.GaussianBlur(img_blur, (5, 5), 0)
-img_blur2 = cv2.GaussianBlur(img_blur1, (5, 5), 0)
-img_blur3 = cv2.GaussianBlur(img_blur2, (5, 5), 0)
+# 畫圖 + 顯示圖片
+img_show = cv2.drawKeypoints(gray, keypoints, img)
 
-showImages(img=img, img_blur1=img_blur1, img_blur2=img_blur2, img_blur3=img_blur3)
-
-"""
-邊緣檢測: 透過 Sobel Filter 實作邊緣檢測
-組合 x-axis, y-axis 的影像合成
-"""
-
-# 對 x 方向做 Sobel 邊緣檢測
-img_sobel_x = cv2.Sobel(gray, cv2.CV_16S, dx=1, dy=0, ksize=3)
-img_sobel_x = cv2.convertScaleAbs(img_sobel_x)
-
-# 對 y 方向做 Sobel 邊緣檢測
-img_sobel_y = cv2.Sobel(gray, cv2.CV_16S, dx=0, dy=1, ksize=3)
-img_sobel_y = cv2.convertScaleAbs(img_sobel_y)
-
-# x, y 方向的邊緣檢測後的圖各以一半的全重進行合成
-img_sobel_combine = cv2.addWeighted(img_sobel_x, 0.5, img_sobel_y, 0.5, 0)
-
-showImages(img=img, img_sobel_x=img_sobel_x, img_sobel_y=img_sobel_y, img_sobel_combine=img_sobel_combine)
-# endregion
-
-# region 作業
-"""
-比較 Sobel 如果在 uint8 的情況下做會 overflow 的狀況
-"""
-
-# 對 x 方向以包含負數的資料格式 (cv2.CV_16S) 進行 Sobel 邊緣檢測
-img_sobel_x = cv2.Sobel(gray, cv2.CV_16S, dx=1, dy=0, ksize=3)
-
-# 對 x 方向依照比例縮放到所有數值都是非負整數
-img_sobel_x = cv2.convertScaleAbs(img_sobel_x)
-
-# 對 x 方向直接以非負整數的資料格式 (uint8) 進行 Sobel 邊緣檢測
-img_sobel_x_uint8 = cv2.Sobel(gray, cv2.CV_8U, dx=1, dy=0, ksize=3)
-
-showImages(gray=gray, img_sobel_x=img_sobel_x, img_sobel_x_uint8=img_sobel_x_uint8)
-
-"""
-比較一次與兩次計算偏微分的結果
-"""
-
-# 求一次導數取得邊緣檢測結果
-img_sobel_x = cv2.convertScaleAbs(cv2.Sobel(gray, cv2.CV_16S, dx=1, dy=0, ksize=3))
-
-# 求二次導數取得邊緣檢測結果
-img_sobel_xx = cv2.convertScaleAbs(cv2.Sobel(img_sobel_x, cv2.CV_16S, dx=1, dy=0, ksize=3))
-
-showImages(gray=gray, img_sobel_x=img_sobel_x, img_sobel_xx=img_sobel_xx)
-# endregion
+showImages(gray=gray, img_show=img_show)
