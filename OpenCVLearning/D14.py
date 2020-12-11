a186f85a9b99542e2b5bab8dea495ef9bef43148
j32u4ukh <j32u4ukh@gmail.com>
2020-12-11 15:57:59 +0800

    D14

---
+++ OpenCVLearning/D14.py
@@ -0,0 +1,54 @@
+from keras.models import Sequential  # 用來啟動 NN
+from keras.layers import Conv2D  # Convolution Operation
+from keras.layers import MaxPooling2D  # Pooling
+from keras.layers import Flatten
+from keras.layers import Dense  # Fully Connected Networks
+from keras.layers import BatchNormalization
+from keras.layers import Activation
+
+"""Working directory: CupoyLearning
+
+『目標』: 搭建 Conv2D-BN-Activation層
+"""
+
+input_shape = (32, 32, 3)
+
+model = Sequential()
+
+# Conv2D-BN-Activation('sigmoid')
+# BatchNormalization主要參數：
+# momentum: Momentum for the moving mean and the moving variance.
+# epsilon: Small float added to variance to avoid dividing by zero.
+
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same', input_shape=input_shape))
+model.add(BatchNormalization(momentum=0.99, epsilon=0.001))
+model.add(Activation('sigmoid'))
+
+# Conv2D-BN-Activation('relu')
+model.add(Conv2D(32, kernel_size=(3, 3), padding='same'))
+model.add(BatchNormalization(momentum=0.99, epsilon=0.001))
+model.add(Activation('relu'))
+
+print(model.summary())
+"""
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #   
+=================================================================
+conv2d_1 (Conv2D)            (None, 32, 32, 32)        896       
+_________________________________________________________________
+batch_normalization_1 (Batch (None, 32, 32, 32)        128       
+_________________________________________________________________
+activation_1 (Activation)    (None, 32, 32, 32)        0         
+_________________________________________________________________
+conv2d_2 (Conv2D)            (None, 32, 32, 32)        9248      
+_________________________________________________________________
+batch_normalization_2 (Batch (None, 32, 32, 32)        128       
+_________________________________________________________________
+activation_2 (Activation)    (None, 32, 32, 32)        0         
+=================================================================
+Total params: 10,400
+Trainable params: 10,272
+Non-trainable params: 128
+_________________________________________________________________
+None
+"""
