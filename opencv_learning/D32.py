3af514c23e58bac322bea5ecf10523f6d33bd5c0
j32u4ukh <j32u4ukh@gmail.com>
2020-12-19 14:54:21 +0800

    D33

---
+++ opencv_learning/D32.py
@@ -0,0 +1,100 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""Working directory: CupoyLearning
+
+YOLO 的核心思想是直接從圖像來預測出 bbox 的坐標以及其類別，具體做法是把圖像切分為 S x S 個網格，
+每個網格預測 B 個 bbox 的信息以及 C 個類別機率，輸出就是 S x S x (5 * B + C) 。
+因為這樣的設計使得 YOLO 檢測流程簡單，在保證一定檢測準確率的前提下，達到 45 fps (TitanX GPU) 的檢測速度。
+"""
+
+
+def show(img):
+    # plt.imshow 預設圖片是 rgb 的
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+
+# 讀取範例圖片
+img = cv2.imread("data/image/yolo_dog.jpg")
+h, w, _ = img.shape
+show(img)
+
+"""假設我們知道這張中汽車、狗和腳踏車的 bboxes 訊息"""
+
+boxes = np.array([[128, 224, 314, 537],
+                  [475, 85, 689, 170],
+                  [162, 119, 565, 441]]).astype(float)
+
+# 把 bboxes 坐標以原圖的 resolution normalize 到 0~1 之間
+boxes[:, [0, 2]] = boxes[:, [0, 2]] / img.shape[1]
+boxes[:, [1, 3]] = boxes[:, [1, 3]] / img.shape[0]
+
+img_show = img.copy()
+
+for x1, y1, x2, y2 in boxes:
+    cv2.rectangle(img_show, (int(x1*w), int(y1*h)), (int(x2*w), int(y2*h)), (0, 255, 0), 2)
+
+show(img_show)
+
+"""假設我們用的是 VOC 資料集, 這是一個標註有 20 類物體的資料集, 
+汽車、狗和腳踏車所對應的 class index 分別是 1, 7, 16"""
+
+labels = np.array([1, 7, 16])
+
+"""有了 bboxes 以及類別的信息，接下來我們就來建構這張圖片在經過 YOLO 網路後應該要輸出的 tensor"""
+
+# 把圖片切成 7x7 的網格
+grid_num = 7
+
+#  B = 2, YOLO 對每個 bbox 有 5 個預測的值(x, y, w, h, confidence), 20 表示論文採用的資料集 PASCAL VOC 中的物件類別數量。
+#  30 = (2 * 5 + 20)
+target = np.zeros((grid_num, grid_num, 30))
+print("YOLO 網路輸出 tensor 的 shape: ", target.shape)
+
+# 主要建構邏輯
+cell_size = 1. / grid_num  # 一個網格的大小
+wh = boxes[:, 2:] - boxes[:, :2]  # bboxes 的 width 以及 height
+cxcy = (boxes[:, 2:] + boxes[:, :2]) / 2  # bboxes 的中心
+
+for i in range(len(boxes)):
+    cxcy_sample = cxcy[i]
+    ij = np.ceil((cxcy_sample / cell_size)) - 1  # bboxes 中心所坐落的網路 index
+    target[int(ij[1]), int(ij[0]), 4] = 1  # 該網格第一個 box 的 confidence 信息
+    target[int(ij[1]), int(ij[0]), 9] = 1  # 該網格第二個 box 的 confidence 信息
+    target[int(ij[1]), int(ij[0]), int(labels[i]) + 9] = 1  # 該網格的類別信息
+    xy = ij * cell_size  # 匹配到的網格的左上角相對坐標
+
+    # 該網格的 x,y,w,h
+    delta_xy = (cxcy_sample - xy) / cell_size
+    target[int(ij[1]), int(ij[0]), 2:4] = wh[i]
+    target[int(ij[1]), int(ij[0]), :2] = delta_xy
+    target[int(ij[1]), int(ij[0]), 7:9] = wh[i]
+    target[int(ij[1]), int(ij[0]), 5:7] = delta_xy
+
+print("顯示 7x7x30 中，第一個 box 的 confidence 信息")
+print(target[:, :, 4])
+
+"""
+上面的 tensor 中, 為 1 的表示那個網格是有物體的, 為 0 就表示沒有物體
+
+我們可以把帶有 7x7 網格的圖畫出來比對一下，
+
+看看汽車、狗和腳踏車的中心是不是恰好就是這個 tensor 中值為 1 的網格呢
+"""
+
+for h in np.arange(0, img.shape[0], img.shape[0]/grid_num).astype(int):
+    cv2.line(img_show, (0, h), (img.shape[1], h), (0, 0, 0), 2)
+for w in np.arange(0, img.shape[1], img.shape[1]/grid_num).astype(int):
+    cv2.line(img_show, (w, 0), (w, img.shape[0]), (0, 0, 0), 2)
+show(img_show)
+
+"""作業
+
+相信今天的簡報還有程式碼範例的信息量已經足夠大了，可以多花時間消化簡報和程式碼範例，
+作業就問一個非常簡單的問題來確保大家有理解到重點，請問在 YOLOv1 的設計下，
+一張圖片最多能檢測出多少個 bbox 呢？
+
+7 * 7 * 2 = 98
+"""
--- opencv_learning/D33.py
+++ opencv_learning/D33.py
@@ -2,14 +2,29 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""Working directory: CupoyLearning"""
+"""Working directory: CupoyLearning
+
+NMS 在 YOLO 的實際運作以每一個類別為主，各別執行NMS。
+YOLO 在NMS中採用的信心度為「每個 bbox 包含各類別的信心度」
+
+作業
+在 NMS 流程中，IoU 重疊率參數(nms_threshold)調高，試著思考一下輸出的預測框會有甚麼變化?
+Hint: 可以回頭看今天介紹的內容，思考輸出的預測框變多或變少?
+
+Ans: 門檻調高，可被保留的預測框數量則會增加
+"""
 
 
-def nms(bounding_boxes, confidence_score, threshold):
+def softNms(bounding_boxes, confidence_score, threshold):
     # If no bounding boxes, return empty list
     if len(bounding_boxes) == 0:
         return [], []
 
+    """預設 list 用來保存 BBOX """
+    picked_boxes = []
+    picked_score = []
+
+    # 所有候選框
     boxes = np.array(bounding_boxes)
 
     '''取出每一個 BOX 的 x1, y1, x2, y2'''
@@ -18,15 +33,11 @@ def nms(bounding_boxes, confidence_score, threshold):
     end_x = boxes[:, 2]
     end_y = boxes[:, 3]
 
-    # Confidence scores of bounding boxes
-    score = np.array(confidence_score)
-
-    '''預設 list 用來保存 BBOX '''
-    picked_boxes = []
-    picked_score = []
-
     '''計算每一個 BOX 的面積'''
     areas = (end_x - start_x + 1) * (end_y - start_y + 1)
+
+    # Confidence scores of bounding boxes
+    score = np.array(confidence_score)
 
     '''排列 BOX 分數'''
     order = np.argsort(score)
@@ -39,18 +50,19 @@ def nms(bounding_boxes, confidence_score, threshold):
         第一輪是全體最大，第二輪則是排除最大以及與最大之重疊率過高的其他框後的最大，以此類推。
         '''
         index = order[-1]
+        others = order[:-1]
 
         # Pick the bounding box with largest confidence score
         '''保存這個 BBOX'''
-        picked_boxes.append(bounding_boxes[index])
-        picked_score.append(confidence_score[index])
+        picked_boxes.append(boxes[index])
+        picked_score.append(score[index])
 
         # Compute ordinates of intersection-over-union(IOU)
         '''計算 Boxes 與最高分 BOX 之間的 IOU'''
-        x1 = np.maximum(start_x[index], start_x[order[:-1]])
-        x2 = np.minimum(end_x[index], end_x[order[:-1]])
-        y1 = np.maximum(start_y[index], start_y[order[:-1]])
-        y2 = np.minimum(end_y[index], end_y[order[:-1]])
+        x1 = np.maximum(start_x[index], start_x[others])
+        x2 = np.minimum(end_x[index], end_x[others])
+        y1 = np.maximum(start_y[index], start_y[others])
+        y2 = np.minimum(end_y[index], end_y[others])
 
         # Compute areas of intersection-over-union
         w = np.maximum(0.0, x2 - x1 + 1)
@@ -59,15 +71,30 @@ def nms(bounding_boxes, confidence_score, threshold):
         print("intersection:", intersection)
 
         ''' 計算和當前 confidence 最高的框的 IOU '''
-        ratio = intersection / (areas[index] + areas[order[:-1]] - intersection)
-        print("ratio:", ratio)
+        iou_ratio = intersection / (areas[index] + areas[others] - intersection)
+        print("iou_ratio:", iou_ratio)
 
-        '''重疊率小於預測 threshold 的 Boxes 要保存下來
-        -> 重疊率過高，表示兩個框圈到同一物體'''
-        left = np.where(ratio < threshold)
+        '''
+        重疊率過高，表示兩個框'可能'圈到同一物體，Soft-NMS 會將該框的分數調降，而非排除
+        
+        B = {b1, b2, ..., bn} 所有候選框(box)
+        S = confidence score
+        M = 當前循環 S 最大的框，將被放入 D 當中
+        D = [] 用於存放選選出來的 box
+        bi = 第 i 個候選框(box)
+        Si = 第 i 個 confidence score
+        Si = Si * e^(-(iou(M, bi)^2 / std))
+        '''
+        std = np.std(iou_ratio) + 1e-8
+        score = score[others]
+        boxes = boxes[others]
+        modify_index = np.where(iou_ratio > threshold)
+        modify = np.ones_like(score)
+        modify[modify_index] = np.exp(-np.power(iou_ratio, 2.0) / std)[modify_index]
+        score *= modify
 
-        # left 的計算過程本來就不包含最後一個，最後一個每次一定都會被移除
-        order = order[left]
+        # 排除當前循環 confidence score 最大的框
+        order = np.argsort(score)
         print(order)
 
     return picked_boxes, picked_score
@@ -78,6 +105,9 @@ path = 'data/image/nms.jpg'
 # Bounding boxes
 bounding_boxes = [(200, 82, 350, 317), (180, 67, 305, 282), (200, 90, 368, 304)]
 confidence_score = [0.9, 0.75, 0.8]
+
+# IoU threshold
+threshold = 0.5
 
 # Read image
 image = cv2.imread(path)
@@ -90,9 +120,6 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 font_scale = 1
 thickness = 2
 
-'''IoU threshold :重疊超過多少就過濾掉'''
-threshold = 0.5
-
 """經過 NMS 之前"""
 
 # Draw bounding boxes and confidence score
@@ -101,6 +128,7 @@ for (start_x, start_y, end_x, end_y), confidence in zip(bounding_boxes, confiden
     cv2.rectangle(org, (start_x, start_y - (2 * baseline + 5)), (start_x + w, start_y), (0, 255, 255), -1)
     cv2.rectangle(org, (start_x, start_y), (end_x, end_y), (0, 255, 255), 2)
     cv2.putText(org, str(confidence), (start_x, start_y), font, font_scale, (0, 0, 0), thickness)
+
 # Show image
 plt.imshow(org[:, :, ::-1])
 plt.show()
@@ -108,7 +136,9 @@ plt.show()
 """經過 NMS 之後"""
 
 # Run non-max suppression algorithm
-picked_boxes, picked_score = nms(bounding_boxes, confidence_score, threshold)
+picked_boxes, picked_score = softNms(bounding_boxes, confidence_score, threshold)
+print("#picked_boxes:", len(picked_boxes))
+# #picked_boxes: 3
 
 # Draw bounding boxes and confidence score after non-maximum supression
 for (start_x, start_y, end_x, end_y), confidence in zip(picked_boxes, picked_score):
