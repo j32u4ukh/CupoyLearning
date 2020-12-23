d7e04e392969a08dbb5379724729183f1022419f
j32u4ukh <j32u4ukh@gmail.com>
2020-12-23 23:38:52 +0800

    D41

---
+++ opencv_learning/D41.py
@@ -0,0 +1,6 @@
+"""Working directory: CupoyLearning
+
+Q: 請敘述，訓練模型時，資料集的格式是什麼？具體一點的說，要提供什麼格式的文件來描述資料集的圖片以及 bboxes 的信息呢？
+
+Ans: [圖片路徑] [依序描述 bboxes 的 X, Y 座標: xmin_1, ymin_1, xmax_1, ymax_1, ..., xmin_n, ymin_n, xmax_n, ymax_n],[類別]
+"""
